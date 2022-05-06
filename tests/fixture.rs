23dd16bfcbb2669f6688b1c2f58071ae45aabab3
modderme123 <modderme123@gmail.com>
2022-05-05 21:46:46 -0700

    get testing working

--- tests/fixture.rs
+++ tests/fixture.rs
@@ -1,12 +1,13 @@
 use std::path::PathBuf;
 
-use jsx_dom_expressions::process_transform;
-use swc_common::{chain, FileName, Mark};
+use jsx_dom_expressions::TransformVisitor;
+use swc_common::{chain, Mark};
 use swc_ecma_transforms_testing::test_fixture;
 use swc_ecmascript::{
     parser::{EsConfig, Syntax},
     transforms::resolver,
 };
+use swc_plugin::ast::as_folder;
 use testing::fixture;
 
 fn syntax() -> Syntax {
@@ -24,10 +25,7 @@ fn jsx_dom_expressions_fixture(input: PathBuf) {
         &|t| {
             chain!(
                 resolver(Mark::new(), Mark::new(), false),
-                process_transform(
-                    t.cm.clone(),
-                    FileName::Real(PathBuf::from("/some-project/src/some-file.js"))
-                )
+                as_folder(TransformVisitor)
             )
         },
         &input,
