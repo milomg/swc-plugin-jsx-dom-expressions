f6b4b6a60eeca776114acd1be1b28601f5223579
Iha Shin <me@xiniha.dev>
2023-01-12 20:51:39 +0900

    Fix import order in fixture

--- tests/fixture/component-with-children/output.js
+++ tests/fixture/component-with-children/output.js
@@ -1,5 +1,5 @@
-import { createComponent as _$createComponent } from "solid-js/web";
 import { template as _$template } from "solid-js/web";
+import { createComponent as _$createComponent } from "solid-js/web";
 const _tmpl$ = /*#__PURE__*/ _$template(`<div>Hello</div>`, 2), _tmpl$2 = /*#__PURE__*/ _$template(`<div>hi</div>`, 2), _tmpl$3 = /*#__PURE__*/ _$template(`<div>wat</div>`, 2);
 const a = ()=>_$createComponent(Comp, {
         get children () {
