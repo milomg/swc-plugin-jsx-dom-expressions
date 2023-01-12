c1f80ecb040ed6d84c0ef8363fa2d991796de8ac
Iha Shin <me@xiniha.dev>
2023-01-12 21:00:17 +0900

    Fix wrong fixture Void elements shouldn't get self-closed ref: https://github.com/ryansolid/dom-expressions/blob/a7a444cd18147a73ae1c514f8defcc0efadf18d2/packages/babel-plugin-jsx-dom-expressions/test/__dom_fixtures__/simpleElements/output.js#L3

--- tests/fixture/void-element/output.js
+++ tests/fixture/void-element/output.js
@@ -1,3 +1,3 @@
 import { template as _$template } from "solid-js/web";
-const _tmpl$ = /*#__PURE__*/ _$template(`<div><img/></div>`, 3);
+const _tmpl$ = /*#__PURE__*/ _$template(`<div><img></div>`, 3);
 const a = ()=>_tmpl$.cloneNode(true);
