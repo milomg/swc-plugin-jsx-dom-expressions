218771e1dc70ceeef7d5ba873b8793ae40ad786e
huuvo <huuvv@grooo.vn>
2023-05-17 11:03:52 +0700

    update test dom fragments

---
+++ tests/fixture/babel/fragments/output.js
@@ -0,0 +1,68 @@
+import { template as _$template } from "r-dom";
+import { setAttribute as _$setAttribute } from "r-dom";
+import { memo as _$memo } from "r-dom";
+import { effect as _$effect } from "r-dom";
+import { createComponent as _$createComponent } from "r-dom";
+const _tmpl$ = /*#__PURE__*/ _$template(`<div>First`), _tmpl$2 = /*#__PURE__*/ _$template(`<div>Last`), _tmpl$3 = /*#__PURE__*/ _$template(`<div>`), _tmpl$4 = /*#__PURE__*/ _$template(`<span>1`), _tmpl$5 = /*#__PURE__*/ _$template(`<span>2`), _tmpl$6 = /*#__PURE__*/ _$template(`<span>3`);
+const multiStatic = [
+    _tmpl$(),
+    _tmpl$2()
+];
+const multiExpression = [
+    _tmpl$(),
+    inserted,
+    _tmpl$2(),
+    "After"
+];
+const multiDynamic = [
+    (()=>{
+        const _el$5 = _tmpl$();
+        _$effect(()=>_$setAttribute(_el$5, "id", state.first));
+        return _el$5;
+    })(),
+    _$memo(()=>state.inserted),
+    (()=>{
+        const _el$6 = _tmpl$2();
+        _$effect(()=>_$setAttribute(_el$6, "id", state.last));
+        return _el$6;
+    })(),
+    "After"
+];
+const singleExpression = inserted;
+const singleDynamic = _$memo(inserted);
+const firstStatic = [
+    inserted,
+    _tmpl$3()
+];
+const firstDynamic = [
+    _$memo(inserted),
+    _tmpl$3()
+];
+const firstComponent = [
+    _$createComponent(Component, {}),
+    _tmpl$3()
+];
+const lastStatic = [
+    _tmpl$3(),
+    inserted
+];
+const lastDynamic = [
+    _tmpl$3(),
+    _$memo(inserted)
+];
+const lastComponent = [
+    _tmpl$3(),
+    _$createComponent(Component, {})
+];
+const spaces = [
+    _tmpl$4(),
+    " ",
+    _tmpl$5(),
+    " ",
+    _tmpl$6()
+];
+const multiLineTrailing = [
+    _tmpl$4(),
+    _tmpl$5(),
+    _tmpl$6()
+];
